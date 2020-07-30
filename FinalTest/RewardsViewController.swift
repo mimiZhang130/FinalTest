c0af5f2844b842ba297c52dfd6a2f3bfc7ec4968
Mimi Zhang <mimi02420@gmail.com>
2020-07-30 17:37:27 -0400

    Second Commit

--- FinalTest/RewardsViewController.swift
+++ FinalTest/RewardsViewController.swift
@@ -10,26 +10,43 @@ import UIKit
 
 class RewardsViewController: UIViewController {
     var previousVC = ViewController()
+    var pointTotal = Int() // pointTotal displayed
     
+    @IBOutlet weak var totalCoins: UILabel!
     @IBOutlet weak var numOfCoins: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let yay = getTotalPoints()
+        print("pointTotal \(yay)")
         // Do any additional setup after loading the view.
         numOfCoins.text = String(previousVC.addPoints.points)
+        totalCoins.text = String(getTotalPoints())
+        
     }
+    func getTotalPoints() -> Int{
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let coreDataPoints = try? context.fetch(PointSystem.fetchRequest()) as? Int{
+                    print("core data point \(coreDataPoints)")
+                    pointTotal = Int(coreDataPoints)
+            }
+        }
+        return pointTotal
+    }
+    
     
 
     
     // MARK: - Navigation
-    
+    /*
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-       
+       if let addVC = segue.destination as? ViewController {
+                    addVC.nextVC = self
+                }
     }
-     
+     */
     
 
 }
--- FinalTest/ViewController.swift
+++ FinalTest/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //var nextVC = RewardsViewController()
+    
     var addPoints = coin()
     var diffLev = Int()
     @IBOutlet weak var goalName: UILabel!
@@ -33,6 +35,15 @@ class ViewController: UIViewController {
     @IBAction func addTapped(_ sender: Any){
          addPoints.points = setPoints()
          print(addPoints.points) //for debugging
+        //Creating core data object to store total number of points that user currently has
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let totPoints = PointSystem(entity: PointSystem.entity(), insertInto: context)
+//            totPoints.totalPoints = Int64(nextVC.pointTotal)
+            totPoints.totalPoints += Int64(addPoints.points)
+            print(totPoints.totalPoints)
+            try? context.save()
+        }
+        
     }
     func setPoints() -> Int{ // setting up point system
         let goal1 = coin() //goal1 is a new coin object, created everytime
@@ -42,6 +53,7 @@ class ViewController: UIViewController {
         goal1.points = goal1.difficultyLevel * goal1.numOfCoins
         return(goal1.points)
     }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
            // Get the new view controller using segue.destination.
